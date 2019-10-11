96f05601befd4f3648872229cef8fba276e7f457
skb1t <chirko.eugene@gmail.com>
2019-10-11 12:48:10 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.Min = min;
+        this.Max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.Min + this.Max ) / 2);
     }
 
     lower() {
-
+        this.Max = Math.ceil((this.Min + this.Max ) / 2)
     }
 
     greater() {
-
+        this.Min = Math.ceil((this.Min + this.Max ) / 2)
     }
 }
 
